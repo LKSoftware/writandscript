92e481b2175d4fbc6a73b35a824a404f5eef0cb5
Leon Kolata <leonkolata@googlemail.com>
2018-12-07 14:04:50 +0100

    dada

--- src/main/java/Hyphenator.java
+++ src/main/java/Hyphenator.java
@@ -4,128 +4,30 @@ import java.util.stream.Collectors;
 
 public class Hyphenator {
 
-    private static final List<Character> vowels = Arrays.asList('a', 'ä', 'e', 'i', 'o', 'ö', 'u', 'ü', 'y');
 
-    private static final List<String> notCuttable = Arrays.asList("ch", "ck", "ph", "rh", "sh", "th", "sch");
 
-    private static final List<String> vowelPairs = Arrays.asList("au", "eu", "ie", "ei", "äu");
-
-    private static final List<Character> consonants = Arrays.asList('b', 'c', 'd', 'f', 'g', 'h', 'j',
-            'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 'ß', 't', 'v', 'w', 'x', 'z');
 
     public static void main(String... args) {
-        //i, e, e, a, e, ä, o, a, e
-        String test = "Dieser Satz enthält Vokale";
-        //System.out.println(getVowels(test));
-        String testSilbe = "haus";//"";
-        String testSilbe2 = "Auetal";
-        Word result = hyphenate(testSilbe);
 
-        System.out.println("Word: " + result.getWord());
-        System.out.println("Word Hyphenated: " + result.getWordHyphenated());
-        System.out.println("Syllables: " + result.getNouns());
+        Word word = new Word("Mauerei");
 
-        Word result2 = hyphenate(testSilbe2);
 
-        System.out.println("Word: " + result2.getWord());
-        System.out.println("Word Hyphenated: " + result2.getWordHyphenated());
-        System.out.println("Syllables: " + result2.getNouns());
+
+        System.out.println("Word: " + word.getWord());
+        System.out.println("Word Hyphenate: " + word.getWordHyphenated());
+        System.out.println("Syllables: " + word.getSyllables());
     }
 
-    public static long getVowels(String text) {
-        return text.chars().mapToObj(s -> (char) s).filter(vowels::contains).count();
+    public void hyphenate(Word word){
+
+
+
     }
 
-    public static Word hyphenate(String word) {
-        List<Character> chars = word.toLowerCase().chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+    private String patternHyphenate(List<Character> chars){
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < chars.size(); i++) {
+        return null;
 
-            if (i + 3 < chars.size()) {
-                //Vokal + Konsonant + gleicher Konsonant + Vokal
-                if (isVowel(chars.get(i)) && chars.get(i + 1).equals(chars.get(i + 2)) && isVowel(chars.get(i + 3))) {
-                    sb.append(chars.get(i)).append(chars.get(i + 1)).append('-').append(chars.get(i + 2)).append(chars.get(i + 3));
-                    i += 3;
-                    continue;
-                }
-                //Vokal + Konsonant + Konsonant + Vokal
-                if (isVowel(chars.get(i)) && isConsonant(chars.get(i + 1)) && isConsonant(chars.get(i + 2)) && isVowel(chars.get(i + 3))) {
-                    sb.append(chars.get(i)).append(chars.get(i + 1)).append('-').append(chars.get(i + 2)).append(chars.get(i + 3));
-                    i += 3;
-                    continue;
-                }
-                //Vokal + Consonant + Vokalpaar
-                if (isVowel(chars.get(i)) && isConsonant(chars.get(i + 1)) && isVowelPair(chars.get(i + 2), chars.get(i + 3))) {
-                    sb.append(chars.get(i)).append(chars.get(i + 1)).append('-').append(chars.get(i + 2)).append(chars.get(i + 3)).append("-");
-                    i += 3;
-                    continue;
-                }
-            }
-            if (i + 2 < chars.size()) {
-                //Vokal + Consonant + Vokal
-                if (isVowel(chars.get(i)) && isConsonant(chars.get(i + 1)) && isVowel(chars.get(i + 2))) {
-
-                    sb.append(chars.get(i)).append('-').append(chars.get(i + 1)).append(chars.get(i + 2));
-                    i += 2;
-                    continue;
-                }
-                //Bei Konsonant + Konsonant + Konsonant vor dem letzten Konsnanten trennen.
-                if (isConsonant(chars.get(i)) && isConsonant(chars.get(i + 1)) && isConsonant(chars.get(i + 2))) {
-                    //"SCH" case
-                    if (notCuttable.contains(chars.get(i).toString() + chars.get(i + 1).toString() + chars.get(i + 2).toString())) {
-                        //Falls nicht am Satzanfang dann trennen.
-                        if (i > 0) {
-                            sb.append('-').append(chars.get(i)).append(chars.get(i + 1)).append(chars.get(i + 2));
-                            i += 2;
-                            continue;
-                        }
-                        sb.append(chars.get(i)).append(chars.get(i + 1)).append(chars.get(i + 2));
-                        i += 2;
-                        continue;
-                    }
-                    sb.append(chars.get(i)).append(chars.get(i + 1)).append('-').append(chars.get(i + 2));
-                    i += 2;
-                    continue;
-                }
-            }
-            if(i + 1 < chars.size())
-            {
-                //Bei Vokalpaaren
-                if(isVowelPair(chars.get(i), chars.get(i + 1)))
-                {
-                    sb.append(chars.get(i)).append(chars.get(i + 1)).append('-');
-                    i += 1;
-                    continue;
-                }
-                //Vokal + Vokal + Konsonant gleicher Konsonant
-                if(isVowel(chars.get(i)) && isVowel(chars.get(i + 1)) && chars.get(i).equals(chars.get(i +1)))
-                {
-                    sb.append(chars.get(i)).append(chars.get(i + 1)).append('-');
-                    i += 1;
-                    continue;
-                }
-            }
-            sb.append(chars.get(i));
-        }
-
-        return new Word(word, sb.toString(), sb.toString().chars().mapToObj(c -> (char)c).filter(c -> c.equals('-')).count() + 1);
     }
 
-
-    private static boolean isVowel(Character s) {
-        return vowels.contains(s);
-    }
-
-    private static boolean isConsonant(Character s) {
-        return consonants.contains(s);
-    }
-
-    private static boolean isVowelPair(Character a, Character b){
-        return vowelPairs.contains(a.toString() + b.toString());
-    }
-
-    private static boolean isNotCuttablePair(String s) {
-        return notCuttable.contains(s);
-    }
 }
--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,14 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Word {
 
-    private final String word;
-    private final String wordHyphenated;
-    private final long syllables;
+    private static final List<Character> VOWELS = Arrays.asList('a', 'ä', 'e', 'i', 'o', 'ö', 'u', 'ü', 'y');
 
-    public Word(String word, String wordHyphenated, long syllable)
+    private static final List<String> CONSONANTPAIRS = Arrays.asList("ch", "ck", "ph", "rh", "sh", "th", "sch");
+
+    private static final List<String> VOWELPAIRS = Arrays.asList("au", "eu", "ie", "ei", "äu");
+
+    private static final List<Character> CONSONANTS = Arrays.asList('b', 'c', 'd', 'f', 'g', 'h', 'j',
+            'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 'ß', 't', 'v', 'w', 'x', 'z');
+
+    private final String word;
+
+    private String wordHyphenated;
+
+    private long syllables;
+
+    private int[] vowelPairsLocation;
+
+    private int vowelPairs = 0;
+    private int vowels = 0;
+
+    public Word(String word)
     {
         this.word = word;
-        this.wordHyphenated = wordHyphenated;
-        this.syllables = syllable;
     }
 
     public String getWord() {
@@ -16,10 +35,87 @@ public class Word {
     }
 
     public String getWordHyphenated() {
-        return wordHyphenated;
+        return wordHyphenated != null ? wordHyphenated : word;
     }
 
-    public long getNouns() {
-        return syllables;
+    public List<Character> getWordAsCharacterList()
+    {
+        return word.toLowerCase().chars().mapToObj(c -> (char) c).collect(Collectors.toList());
     }
+
+    public long getSyllables() {
+        return syllables != 0 ? syllables : count();
+    }
+
+    public int getVowels()
+    {
+        if(vowels == 0)
+        {
+            count();
+        }
+        return vowels;
+    }
+
+    public int getVowelPairs()
+    {
+        if(vowelPairs == 0)
+        {
+            count();
+        }
+        return vowelPairs;
+    }
+
+    public static boolean isVowelPair(List<Character> chars, int i) {
+        return i + 1 < chars.size() && VOWELPAIRS.contains(chars.get(i).toString() + chars.get(i + 1).toString());
+    }
+
+    public static boolean isVowel(Character c)
+    {
+        return VOWELS.contains(c);
+    }
+
+    public static boolean isVowelPair(Character first, Character second)
+    {
+        return VOWELPAIRS.contains(first.toString() + second.toString());
+    }
+
+    public static boolean isConsonant(Character c)
+    {
+        return CONSONANTS.contains(c);
+    }
+
+    public static boolean isConsonantPair(Character... c)
+    {
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(c).forEach(ch -> sb.append(ch.toString()));
+        return CONSONANTPAIRS.contains(sb.toString());
+    }
+
+    private long count()
+    {
+        List<Character> chars = getWordAsCharacterList();
+        vowelPairsLocation = new int[chars.size()];
+        vowelPairs = 0;
+        vowels = 0;
+
+        for (int i = 0 ; i < chars.size(); i++) {
+
+            if(Word.isVowel(chars.get(i)))
+            {
+                if(Word.isVowelPair(chars, i))
+                {
+                    vowelPairs++;
+                    vowelPairsLocation[i] = 1;
+                    vowelPairsLocation[i + 1] = 1;
+                    continue;
+                }
+                if(vowelPairsLocation[i] == 0) {
+                    vowels++;
+                    vowelPairsLocation[i] = 0;
+                }
+            }
+        }
+        return vowels + vowelPairs;
+    }
+
 }
